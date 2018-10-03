53bfe8fc72bcab2d2666cdc8f95d7803d33537a9
Sydney Tolbert <sydneykaturah@lawn-143-215-87-173.lawn.gatech.edu>
2018-10-02 21:06:59 -0400

    progress

--- assets/app.js
+++ assets/app.js
@@ -1,44 +1,37 @@
 $(document).ready(function() {
     var zipCode = 0;
-    var latitude = 0;
-    var longitude = 0;
+    var userLatitude = 0;
+    var userLongitude = 0;
     console.log('hello');
 
-    function getLocation() {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(handleGetCurrentPosition);
-        } 
-        else { 
-            $("#useLocation").text("Geolocation is not supported by this browser.");
-        }
-        console.log("Test");
-    };
-
-    function handleGetCurrentPosition(location){
-        latitude = location.coords.latitude;
-        longitude = location.coords.longitude;
-    };
-
-    $("#submit-landmark").on('click', function(){
-        latitude = '';
-        longitude = '';
+    $("#submit-landmark").on('click', function(event){
+        event.preventDefault();
+        userLatitude = '';
+        userLongitude = '';
         console.log("Search by Landmark")
     });
 
-    $("#submit-zip").on('click', function(){
+    $("#submit-zip").on('click', function(event){
+        event.preventDefault();
         zipCode = $("#inputzip").value;
-        if (zipCode.length == 5) {
+        //if (zipCode.length == 5) {
             
-        }
-        else {
-            $("#inputzip").attr("placeholder", "Please enter a valid 5-digit Zip Code");
-        };
+        //}
+        //else {
+        //    $("#inputzip").attr("placeholder", "Please enter a valid 5-digit Zip Code");
+        //};
         console.log("Search by Zip")
     });
 
-    $("#submit-use-location").on('click', function(){
-        getLocation();
-        console.log("Search by my location");
+    $("#submit-use-location").on('click', function(event){
+        event.preventDefault();
+        console.log("Search by my location")
+        
+        navigator.geolocation.getCurrentPosition(function(position){
+            userLatitude = position.coords.latitude;
+            userLongitude = position.coords.longitude;
+        });
+    
     });
 
     function getShowtimes(){
