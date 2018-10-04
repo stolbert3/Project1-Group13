18ea033f9112100b9f22cfbb71756e3c275c205f
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2018-10-04 19:50:55 -0400

    Latest WIP

--- assets/app.js
+++ assets/app.js
@@ -89,6 +89,7 @@ $("#use-location").on("click", function(event) {
 });
 
 // Document Load Functions =============================================================================================
+/*
 $(document).ready(function() {
     var zipCode = 0;
     var userLatitude = 0;
@@ -113,6 +114,7 @@ $(document).ready(function() {
 
 
 
+*/
 
 // Wikipedia ===========================================================================================================
 function queryWikiAPI(landmark) {
