58a60b6722ce02bcbb0d7ec2ff1200a6951c6d9a
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2018-10-03 23:30:26 -0400

    further work troubleshooting APIs

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,113 @@
+// Functions ===========================================================================================================
+function queryYoutubeAPI(landmark) {
+    let apikey = 'AIzaSyAEIDEiKDoZiRNAKOr0sVZRa3l_KeoyrL8';
+    let queryURL = `https://www.googleapis.com/youtube/v3/search?key=${apikey}&maxResults=25&part=snippet&q=${landmark}&type=video`;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function(response) {
+            /*let snippets = response.items;
+            for (var k = 0; k < snippets.length; k++) {
+                console.log(snippets[k]);
+            }*/
+            let snippets = response.items;
+            for (var k = 0; k < snippets.length; k++) {
+                console.log(snippets[k].id.videoId);
+            }
+
+
+        }).catch(console.log);
+}
+
+
+function queryOMDBAPI(landmark) {
+    let apikey = '39fce93e';
+    let queryURL = `http://www.omdbapi.com/?apikey=${apikey}&s=${landmark}`;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function(response) {
+            let movies = response;
+            console.log(movies);
+
+        }).catch(console.log);
+}
+
+
+function queryMarvelAPI(landmark) {
+    let apikey = '48c2cd340626ba6737ca1d8a4c04bb03';
+    let queryURL = `https://gateway.marvel.com:443/v1/public/characters?name=${landmark}&apikey=${apikey}`;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function(response) {
+            let sh = response;
+            let superhero = response.data;
+            console.log(sh);
+            console.log(superhero);
+
+        }).catch(console.log);
+}
+
+function queryGooglePlaces(landmark) {
+
+    let apikey = 'AIzaSyBHU9bE9A6cB8IKdtFef3n0oIKxNEywZzY';
+    let queryURL = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=${apikey}&input=${landmark}&inputtype=textquery`;
+
+    $.ajax({
+        url: queryURL,
+        contentType: "application/json; charset=UTF-8",
+        method: "GET"
+    })
+        .then(function(response) {
+            let placeID = response.data;
+            console.log(placeID);
+        }).catch(console.log);
+}
+
+function queryAMC(date, lat, long) {
+    console.log(date);
+    console.log(lat);
+    console.log(long);
+
+    $.ajax({
+        method: "GET",
+        url: `https://api.amctheatres.com/v2/showtimes/views/current-location/${date}/${lat}/${long}`,
+        data: {"X-AMC-Vendor-Key": "838420FE-02D5-4D21-B72D-EC76364386E6"},
+        contentType: "application/JSON",
+    })
+        .then(function(response) {
+            let placeID = response.data;
+            console.log(placeID);
+        }).catch(console.log);
+        /*X-AMC-Vendor-Key: `6A3405DB-2F02-46CF-ADAE-E092241F5706`,
+        Content-type: application/JSON; charset=UTF-8,*/
+
+
+    /*curl -X GET -H "X-AMC-Vendor-Key: 6A3405DB-2F02-46CF-ADAE-E092241F5706"
+    -H "Content-Type: application/json"
+    -H "charset: UTF-8" "https://api.amctheatres.com/v2/theatres/610/showtimes/2-24-17/?movie=wall"*/
+
+        /*curl -X GET \
+            -H "X-AMC-Vendor-Key: 6A3405DB-2F02-46CF-ADAE-E092241F5706" \
+            -H "Content-Type:application/json" \
+            -H "charset: UTF-8" \
+            `https://api.amctheatres.com/v2/showtimes/views/current-location/${date}/${lat}/${long}`;*/
+
+        /*curl -u - H "X-AMC-Vendor-Key: 6A3405DB-2F02-46CF-ADAE-E092241F5706"; "Content-Type:application/json"; "charset: UTF-8" -X Get `https://api.amctheatres.com/v2/showtimes/views/current-location/${date}/${lat}/${long}`*/
+
+        /*.then(function(response) {
+            let placeID = response.data;
+            console.log(placeID);
+        }).catch(console.log);*/
+}
+
 // Button Click Functions ==============================================================================================
 $("#login").on("click", function() {
     console.log("login");
@@ -5,13 +115,22 @@ $("#login").on("click", function() {
 
 $("#submit-landmark").on("click", function(event) {
     event.preventDefault();
+    console.log("Search Via Landmark");
+    let landmark = $("#inputLandmark").val().trim();
+    console.log(landmark);
 
-    console.log("submit landmark");
+    queryYoutubeAPI(landmark);
+    /*queryOMDBAPI(landmark);*/
+    /*queryMarvelAPI(landmark);*/
+    /*queryGooglePlaces(landmark);*/
+
+    $("#inputLandmark").val("");
 });
 
 $("#submit-zip").on("click", function(event) {
     event.preventDefault();
     console.log("submit zip");
+
 });
 
 $("#use-location").on("click", function(event) {
@@ -23,7 +142,9 @@ $("#use-location").on("click", function(event) {
         let userLatitude = position.coords.latitude;
         let userLongitude = position.coords.longitude;
 
-        console.log(`Position is ${userLatitude} x ${userLongitude}`)
+        console.log(`Position is ${userLatitude} x ${userLongitude}`);
+        let date = '10-3-2018';
+        /*queryAMC(date, userLatitude, userLongitude)*/
     }
 
     function denied(error) {
@@ -36,6 +157,7 @@ $("#use-location").on("click", function(event) {
         }
         console.log(`Geolocation Error: ${message}`)
     }
+
 });
 
 // Document Load Functions =============================================================================================
