2719a0da433d50a5b9747df1de2295e490003e52
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2018-10-11 16:10:14 -0400

    Minor Code Clean-up and final checks

--- assets/app.js
+++ assets/app.js
@@ -51,8 +51,7 @@ console.log("firebase test");
 let currentMovies = {};
 // Functions ===========================================================================================================
 // To Display the Initial List of All Movies playing nearby.
-function displayMovies(response) {
-    console.log(response);
+function displayMovies() {
 
     let movieCard = `<div class="card text-center border-light bg-transparent">
                         <div class="card-header bg-transparent border-light text-white">Currently Playing Nearby</div>
@@ -70,7 +69,6 @@ function displayMovies(response) {
         
     }
 
-    /*$("#column-2").empty();*/
 }
 
 // To Display the Playing Times of the Movie, once selected.
@@ -86,11 +84,6 @@ function displayShowtimes(key) {
     } else {
         rated = "NA";
     }
-
-   // console.log(ratings);
-   // console.log(r);
-
-   // console.log(rated);
 
     let title = currentMovies[key].title;
     let releaseDate = moment(currentMovies[key].releaseDate).format('D MMM YY');
@@ -152,9 +145,7 @@ function displayShowtimes(key) {
 // To fully reset the page without having to refresh.  Otherwise another movie can simply be selected from the list,
 // and the videos and playing times for it will be shown.
 function resetPage() {
-    let locationSearchCard = `<!--<div class="col-md-4"></div>-->
-                              <!--<div class="col-md-4" id="search-column">-->
-                                <div class="card text-center border-light bg-transparent" id="location-search-card">
+    let locationSearchCard = `<div class="card text-center border-light bg-transparent" id="location-search-card">
                                     <div class="card-header bg-transparent border-light text-white">
                                         Search for Movies
                                     </div>
@@ -177,9 +168,7 @@ function resetPage() {
                                             </div>
                                         </form>
                                     </div>
-                                </div>
-                            <!--</div>-->
-                            <!--<div class="col-md-4"></div>-->`;
+                                </div>`;
 
     $("#search-column").append(locationSearchCard);
 }
